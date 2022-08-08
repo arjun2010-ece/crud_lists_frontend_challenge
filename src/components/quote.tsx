170a92893b4d6e6415dbcf6de54bd624b928d8fc
Arjun Sah <arjun@appliedblockchain.com>
2022-08-08 21:52:42 +0100

    added typecheck for quotelist files

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -1,13 +1,15 @@
-import { FC } from "react";
+import { FC, useState, ChangeEvent, SyntheticEvent, useEffect } from "react";
 import { ReactComponent as EditIcon } from "../icons/edit.svg";
 import { ReactComponent as DeleteIcon } from "../icons/delete.svg";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
 
 type quoteProps = {
   id: string;
   author: string;
   en: string;
   handleDelete: (id: string) => void;
-  handleEdit: (id: string) => void;
+  handleEdit: (quote: any) => void;
 };
 
 const Quote: FC<quoteProps> = ({
@@ -17,18 +19,93 @@ const Quote: FC<quoteProps> = ({
   handleDelete,
   handleEdit,
 }) => {
-  return (
-    <div className="quote-item">
-      <div className="quote">
-        <h3>"{en}"</h3>
-        <p>{author}</p>
-      </div>
+  const [open, setOpen] = useState(false);
+  const [quote, setQuote] = useState({
+    en: "",
+    author: "",
+    id: ""
+  });
 
-      <div className="icons">
-        <DeleteIcon onClick={() => handleDelete(id)} />
-        <EditIcon onClick={() => handleEdit(id)} />
+  useEffect(() => {
+    const selectedQuote = { en, author, id };
+    setQuote(selectedQuote);
+  }, [id]);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  };
+
+  const handleCloseModal = () => setOpen(false);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setQuote({ ...quote, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: SyntheticEvent) => {
+    e.preventDefault();
+    handleEdit(quote)
+    handleCloseModal();
+  };
+
+  return (
+    <>
+      <div className="quote-item">
+        <div className="quote">
+          <h3>"{en}"</h3>
+          <p>{author}</p>
+        </div>
+
+        <div className="icons">
+          <DeleteIcon onClick={() => handleDelete(id)} />
+          <EditIcon onClick={() => handleOpenModal()} />
+        </div>
       </div>
-    </div>
+      {open && (
+        <Modal
+          open={open}
+          onClose={handleCloseModal}
+          center
+          classNames={{
+            modal: "customModal",
+          }}
+        >
+          <h2 className="title">Edit Quote</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>
+                <b>Enter quote</b>
+              </label>
+              <textarea
+                value={quote.en}
+                name="en"
+                className="form-control"
+                rows={4}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>
+                <b>Author</b>
+              </label>
+              <input
+                value={quote.author}
+                name="author"
+                className="form-control"
+                type="text"
+                onChange={handleChange}
+              />
+            </div>
+
+            <button type="submit" className="add-quote">
+              Submit
+            </button>
+          </form>
+        </Modal>
+      )}
+    </>
   );
 };
 
--- src/components/quoteList.tsx
+++ src/components/quoteList.tsx
@@ -2,35 +2,52 @@ import { FC, useEffect, useState } from "react";
 import axios from "axios";
 import Quote from "./quote";
 
+type QuoteType = {
+  id: string
+  en: string
+  author: string
+}
+
 const QuoteList: FC = () => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<QuoteType[]>([]);
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const { data } = await axios.get(process.env.REACT_APP_API as string);
+        const { data } = await axios.get<QuoteType[]>(
+          process.env.REACT_APP_API as string
+        );
         console.log(data);
         setData(data);
-      } catch (error) {
+      } catch (error: any) {
         console.log(error);
       }
     };
-
     getData();
   }, []);
 
   const handleDelete = (id: string) => {
-    let filteredData = data.filter((el : any) => el.id !== id);
-    setData(() => [ ...filteredData])
+    let filteredData = data.filter((el: QuoteType) => el.id !== id);
+    setData(() => [...filteredData]);
   };
 
-  const handleEdit = (id: string) => {
-    console.log(id);
+  const handleEdit = (quote: QuoteType) => {
+    console.log(quote);
+    const updateQuote: QuoteType = quote;
+
+    const updateQuotes: QuoteType[] = data.map((q: QuoteType) => {
+      if (q.id === updateQuote.id) {
+        return updateQuote;
+      }
+      return q;
+    });
+
+    setData(updateQuotes);
   };
 
   let programmingQuotes =
     data &&
-    data.map((quotes: any, i: number) => (
+    data.map((quotes: QuoteType, i: number) => (
       <Quote
         key={i}
         {...quotes}
